08a514a4cec1d42a7a3f12b718471df6e193b6b5
Bob <tawnysoul@MacBook-Pro-28.local>
2020-10-26 20:33:22 +0700

    first commit

---
+++ TestApiClient/TestApiClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApiClient
+//
+//  Created by Bob on 26.10.2020.
+//  Copyright © 2020 Bob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
